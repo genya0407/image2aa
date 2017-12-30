5ac24633e0d0656abd01c1e9a3881f9ad87a773b
Yusuke Sangenya <longinus.eva@gmail.com>
2017-12-31 04:08:10 +0900

    add option

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 extern crate image;
 #[macro_use] extern crate ndarray;
 extern crate string_error;
+extern crate getopts;
 
 use image::{ImageDecoder, DecodingResult, ColorType};
 use std::fs::File;
@@ -11,6 +12,7 @@ use ndarray::*;
 use std::error::Error;
 use string_error::{static_err};
 use std::env;
+use getopts::Options;
 
 mod filter;
 
@@ -22,7 +24,11 @@ fn read_png(filename: String) -> Result<Array3<f32>, Box<Error>> {
     match result {
         DecodingResult::U8(v) => {
             let arr = Array1::<f32>::from(v.into_iter().map(|e| e as f32).collect::<Vec<f32>>());
-            arr.into_shape((y as Ix, x as Ix, 4 as Ix)).map_err(|_| static_err("Wrong shape!"))
+            match decoder.colortype() {
+                Ok(ColorType::RGBA(_)) => arr.into_shape((y as Ix, x as Ix, 4 as Ix)).map_err(|_| static_err("Wrong shape!")),
+                Ok(ColorType::RGB(_)) => arr.into_shape((y as Ix, x as Ix, 3 as Ix)).map_err(|_| static_err("Wrong shape!")),
+                _ => Err(static_err("Unsupported colortype")),
+            }
         }
         DecodingResult::U16(_) => Err(static_err("Unsupported bit depth!"))
     }
@@ -42,14 +48,43 @@ fn write_grayscale_png(filename: String, img: &Array2<f32>) -> Result<(), Box<Er
     ).map_err(|_| static_err("Error writing image!"))
 }
 
+fn setup_option_parser() -> Options {
+    let mut opts = Options::new();
+    opts.optopt("s", "blocksize", "set bocksize (default: 32)", "SIZE");
+    opts.reqopt("i", "input", "input file path", "FILE");
+    opts.optopt("", "char-detect-thresh", "threshould for character detection (default: 10)", "THRESH");
+    opts.optopt("", "line-detect-thresh", "threshould for line detection (default: 10)", "THRESH");
+    return opts;
+}
+
 fn main() {
-    let input_file = env::args().nth(1).unwrap();
+    let args: Vec<String> = env::args().collect();
+    let parser = setup_option_parser();
+    let matches = match parser.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string())
+    };
+
+    let input_file = matches.opt_str("i").unwrap();
+
+    let mut hough_filter = filter::block_hough::default();
+    if let Some(block_size_str) = matches.opt_str("s") {
+        hough_filter.block_size = block_size_str.parse().unwrap();
+    }
+    if let Some(slope_count_thresh_str) = matches.opt_str("char-detect-thresh") {
+        hough_filter.slope_count_thresh = slope_count_thresh_str.parse().unwrap();
+    }
+
+    let mut binary_filter = filter::binary::default();
+    if let Some(line_detect_thresh_str) = matches.opt_str("line-detect-thresh") {
+        binary_filter.thresh = line_detect_thresh_str.parse().unwrap();
+    }
+
     let image_array = read_png(input_file).unwrap();
     let grayscale_array = filter::grayscale::default().run(image_array);
     let gradient_array = filter::line::default().run(grayscale_array.clone());
-    let line_array = filter::binary::default().run(gradient_array);
+    let line_array = binary_filter.run(gradient_array);
     write_grayscale_png(String::from("out/line.png"), &line_array).unwrap();
-    let hough_filter = filter::block_hough::default();
     let hough_array = hough_filter.run(line_array);
     let aa = filter::ascii_art::default().run(hough_array);
     println!("{}", aa);
