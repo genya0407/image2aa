6a37b339304b06c6b55e9d4b1f15f03ea1baaf23
Yusuke Sangenya <longinus.eva@gmail.com>
2018-01-28 03:58:55 +0900

    jpeg対応

--- src/utils.rs
+++ src/utils.rs
@@ -4,7 +4,7 @@ use ndarray::*;
 use image::{ImageDecoder, DecodingResult, ColorType};
 use string_error::{static_err};
 use std::error::Error;
-use std::io::{Read, Write};
+use std::io::{Read, Write, Cursor};
 
 pub fn convolve2d(base_arr: &Array2<f32>, filter: &Array2<f32>) -> Array2<f32> {
     let ys: isize = base_arr.shape()[0] as isize;
@@ -18,23 +18,40 @@ pub fn convolve2d(base_arr: &Array2<f32>, filter: &Array2<f32>) -> Array2<f32> {
     return result;
 }
 
-pub fn read_png<R: Read>(image_file: R) -> Result<Array3<f32>, Box<Error>> {
-    let decoder = image::png::PNGDecoder::new(image_file);
-    read_image(decoder)
-}
-
-pub fn read_image<D:ImageDecoder>(mut decoder: D) -> Result<Array3<f32>, Box<Error>> {
-    let result = decoder.read_image()?;
-    let (x, y) = decoder.dimensions()?;
+pub fn read_image<R: Read>(mut image_file: R) -> Result<Array3<f32>, Box<Error>> {
+    let mut image_buffer = Vec::new();
+    image_file.read_to_end(&mut image_buffer)?;
+    let format = image::guess_format(&image_buffer)?;
+    let image_reader = Cursor::new(image_buffer);
+    let abstract_image_data = match format {
+        image::ImageFormat::PNG => {
+            let mut decoder = image::png::PNGDecoder::new(image_reader);
+            let result = decoder.read_image()?;
+            let (x, y) = decoder.dimensions()?;
+            let colortype = decoder.colortype()?;
+            Ok((result, (x, y), colortype))
+        },
+        image::ImageFormat::JPEG => {
+            let mut decoder = image::jpeg::JPEGDecoder::new(image_reader);
+            let result = decoder.read_image()?;
+            let (x, y) = decoder.dimensions()?;
+            let colortype = decoder.colortype()?;
+            Ok((result, (x, y), colortype))
+        },
+        _ => Err(static_err("Unsupported file type. Only PNG and JPEG are supported.")),
+    };
+    let (result, (x, y), colortype) = abstract_image_data?;
+    
     let raw_data = match result {
         DecodingResult::U8(v) => v.into_iter().map(|e| e as f32).collect::<Vec<f32>>(),
         DecodingResult::U16(v) => v.into_iter().map(|e| e as f32).collect::<Vec<f32>>(),
     };
     let arr = Array1::<f32>::from(raw_data);
-    match decoder.colortype() {
-        Ok(ColorType::RGBA(_)) =>
+
+    match colortype {
+        ColorType::RGBA(_) =>
             arr.into_shape((y as Ix, x as Ix, 4 as Ix)).map_err(|_| static_err("Wrong shape!")),
-        Ok(ColorType::RGB(_)) =>
+        ColorType::RGB(_) =>
             arr.into_shape((y as Ix, x as Ix, 3 as Ix)).map_err(|_| static_err("Wrong shape!")),
         _ => Err(static_err("Unsupported colortype")),
     }
